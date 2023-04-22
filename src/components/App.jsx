0a10cfdbb7e2cc1e756c38653048c762457e12c0
Demag0g1 <konievanatol@gmail.com>
2023-04-22 21:23:10 +0300

    hw for check

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { SectionWrap } from './section/Section';
+import { FeedbackOptions } from './feedback/FeedbackOption';
+import { Notification } from './notification/Notification';
+import { Statistics } from './statistic/statistic';
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  updateState = nameFeedback => {
+    this.setState(oldData => {
+      let obj = { ...oldData };
+      obj[nameFeedback] = oldData[nameFeedback] + 1;
+      return obj;
+    });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.floor(
+      (this.state.good /
+        (this.state.good + this.state.neutral + this.state.bad)) *
+        100 || 0
+    );
+  };
+
+  render() {
+    return (
+      <div className={css.container}>
+        <SectionWrap title="Please Leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeavefeedback={this.updateState}
+          />
+        </SectionWrap>
+
+        <SectionWrap title="Statistics">
+          {this.countTotalFeedback() === 0 ? (
+            <Notification message="There is no feedback yet..." />
+          ) : (
+            <Statistics
+              options={Object.keys(this.state)}
+              statistic={this.state}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage}
+            />
+          )}
+        </SectionWrap>
+      </div>
+    );
+  }
+}
